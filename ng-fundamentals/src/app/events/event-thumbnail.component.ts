e4502d0b5752918cbb80781f9ca3c0e98ecc2016
LourenceQ <lawrenceqf@gmail.com>
2023-08-22 20:39:26 -0300

    implenetando component vav-bar

--- ng-fundamentals/src/app/events/event-thumbnail.component.ts
+++ ng-fundamentals/src/app/events/event-thumbnail.component.ts
@@ -4,20 +4,23 @@ import { Component, Input } from "@angular/core";
     selector: 'event-thumbnail',
     template: `
     <div class="well hoverwell thunbnail">
-        <h2>{{event.name}}</h2>
-         <div>Date: {{event.date}}</div>
-         <div>Time: {{event.time}}</div>
-         <div>Price:\${{event.price}}</div>
-         <div>
-            <span>Location: {{event.location.address}}</span>
-            <span>&nbsp;</span>
-            <span>{{event.location.city}}, {{event.location.country}}</span>
-         </div>
+      <h2>{{event.name}}</h2>
+        <div>Date: {{event.date}}</div>
+        <div>Time: {{event.time}}</div>
+        <div>Price: \${{event.price}}</div>
+        <div>
+          <span>Location: {{event.location.address}}</span>
+          <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+        </div>
     </div>
-    `
+    `,
+    styles:[`
+    .pad-left { margin-left: 10px; }
+    .well div { color: #bbb; }
+    `]
 })
 
 export class EventThumbnailComponent {
-    @Input() event:any
+@Input() event:any
 
 }
--- ng-fundamentals/src/app/events/events-list.component.ts
+++ ng-fundamentals/src/app/events/events-list.component.ts
@@ -3,26 +3,26 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'events-list',
     template: `
-        <div>
-            <h1>Upcomming Angualr Events</h1>
-            <hr/>
-            <event-thumbnail [event]="event1"></event-thumbnail>
-        </div>
+      <div>
+        <h1>Upcomming Angualr Events</h1>
+        <hr/>
+        <event-thumbnail  [event]="event1"></event-thumbnail>
+      </div>
     `
 })
 
 export class EventListComponent {
-    event1 = {
-        id: 1,
-        name: 'Angular Connect',
-        date: '9/26/2023',
-        time: '10:00 am',
-        price: 600,
-        imageUrl: '/assets/images/angularconnect-shield',
-        location: {
-            address: '1057 DT',
-            city: 'London',
-            country: 'England'
-        }
+  event1 = {
+    id: 1,
+    name: 'Angular Connect',
+    date: '9/26/2023',
+    time: '10:00 am',
+    price: 600,
+    imageUrl: '/assets/images/angularconnect-shield',
+    location: {
+      address: '1057 DT',
+      city: 'London',
+      country: 'England'
     }
+  }
 }
