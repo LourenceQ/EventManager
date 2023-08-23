7af3459930b2c01b5d300d229b06e150cfbd6f66
LourenceQ <lawrenceqf@gmail.com>
2023-08-23 11:32:27 -0300

    Implementando diretiva ngFor para renderização de eventos

--- ng-fundamentals/src/app/events/event-thumbnail.component.ts
+++ ng-fundamentals/src/app/events/event-thumbnail.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from "@angular/core";
 @Component({
     selector: 'event-thumbnail',
     template: `
-    <div class="well hoverwell thunbnail">
+    <div class="well hoverwell thumbnail">
       <h2>{{event.name}}</h2>
         <div>Date: {{event.date}}</div>
         <div>Time: {{event.time}}</div>
@@ -14,9 +14,10 @@ import { Component, Input } from "@angular/core";
         </div>
     </div>
     `,
-    styles:[`
-    .pad-left { margin-left: 10px; }
-    .well div { color: #bbb; }
+    styles: [`
+      .thumbnail {min-height: 210px;}
+      .pad-left {margin-left:10px;}
+      .well div {color: #bbb;}
     `]
 })
 
