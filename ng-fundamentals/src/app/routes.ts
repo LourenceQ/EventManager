49bd4f2e7e6ec58e09e8216a46cbbe05fed02a2f
LourenceQ <lawrenceqf@gmail.com>
2023-08-28 21:51:22 -0300

    implementando pagina new component, rotas e evenet guard

--- ng-fundamentals/src/app/routes.ts
+++ ng-fundamentals/src/app/routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from "@angular/router";
 import { EventDetailsComponent } from "./events/event-details/event-details.component";
 import { EventListComponent } from "./events/events-list.component";
+import { CreateEventComponent } from "./events/create-event.component";
+import { Error404Component } from "./errors/404.component";
 
 export const appRoutes: Routes = [
+    { path: 'events/new', component:CreateEventComponent },
     { path: 'events', component: EventListComponent },
     { path: 'events/:id', component: EventDetailsComponent },
+    { path: '404', component: Error404Component },
     { path: '', redirectTo: '/events', pathMatch: 'full' }
 ]
