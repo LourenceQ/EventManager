2978c16a69705513910cdcb7636a78daf92d11ac
LourenceQ <lawrenceqf@gmail.com>
2023-09-11 07:23:07 -0300

    Implementando método para atualizar usuario

--- ng-fundamentals/src/app/user/profile.component.ts
+++ ng-fundamentals/src/app/user/profile.component.ts
@@ -1,18 +1,34 @@
 import { Component, OnInit } from '@angular/core'
 import { FormGroup, FormControl } from '@angular/forms'
-
+import { AuthService } from './auth.service'
+import { Router } from '@angular/router'
 @Component({
   templateUrl: './profile.component.html',
 })
 export class ProfileComponent implements OnInit{
   profileForm: FormGroup
 
+  constructor(private authService:AuthService
+    , private router: Router) {
+
+  }
+
   ngOnInit(): void {
-    let firstName = new FormControl()
-    let lastName = new FormControl()
+    let firstName = new FormControl(this.authService.currentUser.firstName)
+    let lastName = new FormControl(this.authService.currentUser.lastName)
     this.profileForm = new FormGroup({
       firstName: firstName,
       lastName: lastName
     })
   }
+
+  cancel() {
+    this.router.navigate(['events'])
+  }
+
+  saveProfile(formValues) {
+    this.authService.updateCurrrentUser(formValues.firstName, formValues.lastName)
+    this.router.navigate(['events'])
+  }
+
 }
