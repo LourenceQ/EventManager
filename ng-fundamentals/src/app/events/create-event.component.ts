1a83f75217037078d7925e5199bb5414bbbf6ed1
LourenceQ <lawrenceqf@gmail.com>
2023-09-13 21:03:57 -0300

    implementando form de create event

--- ng-fundamentals/src/app/events/create-event.component.ts
+++ ng-fundamentals/src/app/events/create-event.component.ts
@@ -3,26 +3,46 @@ import { Router } from "@angular/router";
 
 
 @Component({
-  template: `
-    <h1>New Event</h1>
-    <hr>
-    <div class="col-md-6">  
-      <h3>[Create event form will go here]</h3>
-      <br/>
-      <br/>
-      <button type="submit" class="btn btn-primary">Save</button>
-      <button type="button" class="btn btn-default"
-        (click)="cancel()">Cancel</button>
-    </div>
-  `
+  templateUrl: './create-event.component.html'
+  , styles: [
+    `
+      em {
+        float: right;
+        color: #E05C65;
+        padding-left: 10px;
+      }
+      .error input {
+        background-color: #E3C3C5;
+      }
+      .error ::-webkit-input-placeholder {
+        color: #999;
+      }
+      .error ::-moz-placeholder {
+        color: #999;
+      }
+      .error :-moz-placeholder {
+        color: #999;
+      }
+      .error :ms-input-placeholder {
+        color: #999;
+      }
+    `,
+  ]
 })
 
 
 export class CreateEventComponent {
+  newEvent
   isDirt: boolean = true
   constructor(private router:Router) {
 
   }
+
+  saveEvent(formValues) {
+    console.log(formValues)
+  }
+
+
   cancel() {
     this.router.navigate(['/events'])
   }
