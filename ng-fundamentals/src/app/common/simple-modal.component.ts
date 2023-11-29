2247ed8fd6cb650ea9011cc4d21ef7afcd252976
LourenceQ <lawrenceqf@gmail.com>
2023-11-29 07:58:46 -0300

    implementando funcionalidade para fechar modal

--- ng-fundamentals/src/app/common/simple-modal.component.ts
+++ ng-fundamentals/src/app/common/simple-modal.component.ts
@@ -1,9 +1,9 @@
-import { Component, Input } from '@angular/core'
-
+import { Component, Input, ViewChild, ElementRef, Inject } from '@angular/core'
+import { JQ_TOKEN } from './jQuery.service'
 @Component({
     selector: 'simple-modal',
     template: `
-    <div id="{{elementId}}" class="modal fade" tabindex="-1">
+    <div id="{{elementId}}" #modalContainer class="modal fade" tabindex="-1">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -12,7 +12,7 @@ import { Component, Input } from '@angular/core'
                     </button>
                     <h4 class="modal-title">{{title}}</h4>
                 </div>
-                <div class="modal-body">
+                <div class="modal-body" (click)="closeModal()">
                     <ng-content></ng-content>
                 </div>
             </div> 
@@ -26,4 +26,14 @@ import { Component, Input } from '@angular/core'
 export class SimpleModalComponent {
     @Input() title: string;
     @Input() elementId: string;
+    @ViewChild('modalContainer') containerEl: ElementRef
+    @Input() closeOnBodyClick: string
+
+    constructor(@Inject(JQ_TOKEN) private $: any) {}
+
+    closeModal() {
+        if(this.closeOnBodyClick.toLocaleLowerCase() === 'true') {
+            this.$(this.containerEl.nativeElement).modal('hide')
+        }        
+    }
 }
