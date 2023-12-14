5a4240feebf30b455627a5cd805ac9e3cf2e3d39
LourenceQ <lawrenceqf@gmail.com>
2023-12-14 19:12:42 -0300

    refatorando funcionalidade addvoter

--- ng-fundamentals/src/app/events/event-details/session-list.component.ts
+++ ng-fundamentals/src/app/events/event-details/session-list.component.ts
@@ -12,6 +12,7 @@ export class SessionListComponent implements OnChanges{
   @Input() sessions: ISession[]
   @Input() filterBy: string
   @Input() sortBy: string
+  @Input() eventId: number
   visibleSessions: ISession[] = []
 
   constructor(private auth: AuthService, private voterService: VoterService) {}
@@ -33,7 +34,7 @@ export class SessionListComponent implements OnChanges{
     if(this.userHasVoted(session))
       this.voterService.deleteVoter(session, this.auth.currentUser.userName)
     else 
-      this.voterService.addVoter(session, this.auth.currentUser.userName)
+      this.voterService.addVoter(this.eventId, session, this.auth.currentUser.userName)
     if(this.sortBy === 'votes')
       this.visibleSessions.sort(sortByVotesDesc)
   }
