049e46023944a12c4ea1a6265f6fa457eda16252
LourenceQ <lawrenceqf@gmail.com>
2023-12-29 19:27:23 -0300

    finalização de primeiro teste de integração

--- ng-fundamentals/src/app/events/event-details/session-list.component.integrated.spec.ts
+++ ng-fundamentals/src/app/events/event-details/session-list.component.integrated.spec.ts
@@ -3,6 +3,7 @@ import { SessionListComponent } from "./session-list.component";
 import { DebugElement } from "@angular/core";
 import { AuthService } from "src/app/user/auth.service";
 import { VoterService } from "./voter.service";
+import { DurationPipe } from "../shared";
 
 describe('SessionListComponent', () => {
   
@@ -14,9 +15,13 @@ describe('SessionListComponent', () => {
   let debugEl: DebugElement
 
   beforeEach(() => {
+    mockAuthService = { isAuthenticated: () => true, currentUser: { userName: 'Joe'} }
+    mockVoterService = { userHasVoted: () => true }
+
     TestBed.configureTestingModule({
       declarations: [
         SessionListComponent
+        , DurationPipe
       ],
       providers: [
         { provide: AuthService, useValue: mockAuthService},
@@ -28,9 +33,33 @@ describe('SessionListComponent', () => {
     component = fixture.componentInstance
     debugEl = fixture.debugElement
     element = fixture.nativeElement
+
   })
 
   describe('initial display', () => {
+
+    it('should have the correct name', () => {
+      component.sessions = [
+        { 
+          name: 'Session 1'
+          , id: 3
+          , presenter: 'Joe'
+          , duration: 1
+          , level: 'beginner'
+          , abstract: 'abstract'
+          , voters: ['john', 'bob'] 
+        }
+      ]
+      component.filterBy = 'all'
+      component.sortBy = 'name'
+      component.eventId = 4
+      component.ngOnChanges()
+
+      fixture.detectChanges()
+
+      expect(element.querySelector('[well-title]').textContent)
+        .toContain('Session 1')
+    })
     
   })  
-});
+})
